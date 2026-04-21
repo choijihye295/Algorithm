b950458affe14fffc20790503929798f723c98e9
최지혜 <choijihye295@jbnu.ac.kr>
2026-04-21 15:33:58 +0900

    Update 강원도에위치한 생산공장 목록 출력하기.sql

--- SQL/SELECT/강원도에 위치한 생산공장 목록 출력하기.sql
+++ SQL/SELECT/강원도에 위치한 생산공장 목록 출력하기.sql
@@ -1,4 +1,4 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/131112
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131112
 
 -- 코드를 입력하세요
 SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
