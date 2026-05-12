21b25e17676eed7fb71192c0fb9bd0a9efc53b1d
최지혜 <choijihye295@gmail.com>
2026-05-12 10:08:09 +0900

    Create 주식가격.java

---
+++ 프로그래머스/KB/주식가격.java
@@ -0,0 +1,21 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42584
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int len = prices.length;
+        int[] answer = new int[len];
+        
+        for(int i=0; i<len; i++){
+            int sum=0;
+            for(int j=i+1; j<len; j++){
+                sum++;
+                if(prices[i]>prices[j]){
+                    break;
+                }
+            }
+            answer[i]=sum;
+        }
+        
+        return answer;
+    }
+}
