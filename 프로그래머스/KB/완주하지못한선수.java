30d8446a3a1c05c4700587ea51d1c1cc2db21a7f
최지혜 <choijihye295@gmail.com>
2026-05-12 13:50:40 +0900

    Create 완주하지못한선수.java

---
+++ 프로그래머스/KB/완주하지못한선수.java
@@ -0,0 +1,29 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42576
+
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<participant.length; i++){
+            //참가자 +1
+            map.put(participant[i], map.getOrDefault(participant[i], 0)+1);
+            
+        }
+        for(int i=0; i<completion.length; i++){
+            //완주자 0
+            map.put(completion[i], map.getOrDefault(completion[i], 0)-1);
+            
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
